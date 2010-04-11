bdc471bc287a1b3e0d0173fcef9766d12470c363
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2010-04-11 09:43:03 +0000

    Remove unused function and import call.
    git-svn-id: http://svn.temoaproject.org/temoa/trunk@23 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- SetTech.py
+++
@@ -1,8 +0,0 @@
-def TechSet_Init ( model ):
-	ans = []
-	ans.extend( [ i for i in model.tech_base ] )
-	ans.extend( [ i for i in model.tech_shoulder ] )
-	ans.extend( [ i for i in model.tech_peak ] )
-
-	return ans
-
--- set_init.py
+++ set_init.py
@@ -2,6 +2,5 @@ from SetOperatingPeriod  import *
 from SetPeriod           import *
 from SetSegment          import *
 from SetSlice            import *
-from SetTech             import *
 from SetInvestmentPeriod import *
 
