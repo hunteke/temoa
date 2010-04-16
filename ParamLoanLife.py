ab250fad77e19866301aeb05ac8817e974568bd5
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2010-04-16 20:29:18 +0000

    Thinko/typo fix in Parameter construction
    Would help if I were to actually compare the passed in values
    
    
    git-svn-id: http://svn.temoaproject.org/temoa/trunk@28 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- ParamLoanLife.py
+++ ParamLoanLife.py
@@ -3,10 +3,10 @@ import debug as D
 def LoanLifeParam_Init ( tech, model ):
 	D.write( D.INFO, "LoanLifeParam_Init parameter initialization\n" )
 
-	if   'coal'     : return 20
-	elif 'nuclear'  : return 40
-	elif 'wind_ons' : return 10
-	elif 'solar_pv' : return 15
+	if   'coal'     == tech: return 20
+	elif 'nuclear'  == tech: return 40
+	elif 'wind_ons' == tech: return 10
+	elif 'solar_pv' == tech: return 15
 
 	return 10
 
--- ParamTechLife.py
+++ ParamTechLife.py
@@ -1,13 +1,13 @@
 import debug as D
 
 def TechLifeParam_Init ( tech, model ):
-	D.write( D.INFO, "LoanLifeParam_Init parameter initialization\n" )
+	D.write( D.INFO, "TechLifeParam_Init parameter initialization\n" )
 
 	# return years (10 years = 1 period)
-	if   'coal'     : return 30
-	elif 'nuclear'  : return 40
-	elif 'wind_ons' : return 20
-	elif 'solar_pv' : return 25
+	if   'coal'     == tech: return 30
+	elif 'nuclear'  == tech: return 40
+	elif 'wind_ons' == tech: return 20
+	elif 'solar_pv' == tech: return 30
 
-	return 10
+	return 20
 
