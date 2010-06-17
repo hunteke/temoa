bbd78b7e57a8cb85ba4715f74f52682a9c7b8dee
sarat <sarat@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2010-06-17 03:34:49 +0000

    Updated script to upload autogenerated documentation to website.
    git-svn-id: http://svn.temoaproject.org/temoa/trunk@38 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- sphinx/run.sh
+++ sphinx/run.sh
@@ -3,7 +3,7 @@ make clean; make html
 echo "Copied new documentation to ./html sub-directory"
 cp -r _build/html html 
 
-echo "Copying new documentation to the temoaproject.org website"
+echo -e "\n\nCopying new documentation to the temoaproject.org website\n\n"
 scp -r html energym@energy-modeling.org:temoaproject.org/docs 
 echo "Documentation updated on the website."
-echo "Please visit temoaproject.org/docs to check."
+echo -e "\n\n Please visit http://temoaproject.org/docs to check.\n\n"
