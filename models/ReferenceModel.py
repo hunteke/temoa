394af22870917ab58f711063f37ff906d613b9d1
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2010-10-21 14:39:04 +0000

    Finish period -> operating_period bugfix
    Whoops!  I thought I'd already committed this before the previous commit.
    
    
    git-svn-id: http://svn.temoaproject.org/temoa/branches/stochastic@80 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- models/ReferenceModel.py
+++ models/ReferenceModel.py
@@ -44,22 +44,22 @@ model.tech_existing_by_seg = Set( model.segment )
 
 model.period  = Set()
 
-model.invest_period = Set( within=model.period, initialize=SetPeriod_Init )
+model.invest_period    = Set( ordered=True, within=model.period, initialize=SetOperatingPeriod_Init )
 model.operating_period = Set( ordered=True, within=model.period, initialize=SetOperatingPeriod_Init )
 
 model.inter_period = Param( model.operating_period, initialize=ParamInterPeriod_Init )
-model.urn_price = Param( model.period )
-model.coal_price = Param( model.period )
-model.diesel_price = Param( model.period )
-model.ng_price = Param( model.period )
+model.urn_price    = Param( model.operating_period )
+model.coal_price   = Param( model.operating_period )
+model.diesel_price = Param( model.operating_period )
+model.ng_price     = Param( model.operating_period )
 
 model.tech_life  = Param( model.tech_all, initialize=TechLifeParam_Init )
 model.loan_life  = Param( model.tech_new, initialize=LoanLifeParam_Init )
 
 model.investment = Set( initialize=SetInvestmentPeriod_Init, dimen=3 ) # imat
-model.vintage    = Param( model.tech_all, model.invest_period, model.period, initialize=VintagePeriodParam_Init )    # vmat
+model.vintage    = Param( model.tech_all, model.invest_period, model.operating_period, initialize=VintagePeriodParam_Init )    # vmat
 
-model.co2_tot     = Param( model.period )  # Total annual CO2 emissions in MmtCO2
+model.co2_tot     = Param( model.operating_period )  # Total annual CO2 emissions in MmtCO2
 model.cost_target = Param()
 model.global_discount_rate = Param()
 
@@ -70,12 +70,12 @@ model.winds_off_max_total = Param()
 model.solar_th_max_total  = Param()
 
 
-model.power_dmd   = Param( model.period, model.segment ) # installed capacity (GW) required
-model.energy_dmd  = Param( model.period, model.segment ) # electricity generation (GWh) required
+model.power_dmd   = Param( model.operating_period, model.segment ) # installed capacity (GW) required
+model.energy_dmd  = Param( model.operating_period, model.segment ) # electricity generation (GWh) required
 
-model.investment_costs = Param( model.tech_new, model.invest_period, model.period, default=0 )     # C_i "technology investment cost"
-model.fixed_costs      = Param( model.tech_all, model.invest_period, model.period, default=0 )      # C_f "technology fixed cost"
-model.marg_costs       = Param( model.tech_all, model.period, initialize=MarginalCostsParam_Init ) # C_m "technology marginal cost"
+model.investment_costs = Param( model.tech_new, model.invest_period, model.operating_period, default=0 )     # C_i "technology investment cost"
+model.fixed_costs      = Param( model.tech_all, model.invest_period, model.operating_period, default=0 )      # C_f "technology fixed cost"
+model.marg_costs       = Param( model.tech_all, model.operating_period, initialize=MarginalCostsParam_Init ) # C_m "technology marginal cost"
 model.cf_max           = Param( model.tech_all )
 model.ratio            = Param( model.tech_all )
 model.co2_factors      = Param( model.tech_all )
@@ -86,22 +86,23 @@ model.t0_capacity      = Param( model.operating_period, model.tech_existing )
 model.loan_cost     = Param( model.tech_new, rule=ParamLoanCost_Init )
 model.period_spread = Param( model.operating_period, rule=ParamPeriodSpread_Init )
 
-model.xc = Var( model.tech_new, model.invest_period,               within=NonNegativeReals, bounds=(0, 1e50) )
-model.xu = Var( model.tech_all, model.invest_period, model.period, within=NonNegativeReals, bounds=(0, 1e50) )
+model.xc = Var( model.tech_new, model.invest_period,                         within=NonNegativeReals, bounds=(0, 1e50) )
+model.xu = Var( model.tech_all, model.invest_period, model.operating_period, within=NonNegativeReals, bounds=(0, 1e50) )
 
 model.PeriodCost = Var( model.operating_period, within=NonNegativeReals )
 
 # Objective function(s)
 def TotalCost ( model ):
-    return summation( model.PeriodCost )
+	return sum( model.PeriodCost[ p ] for p in model.operating_period )
 model.Total_Cost = Objective( rule=TotalCost, sense=minimize )
+
 
 
 # Constraints
 model.Energy_Demand = Constraint( model.segment, model.operating_period, rule=Energy_Demand )
 model.Capacity      = Constraint( model.segment, model.operating_period, rule=Capacity_Req )
 
-model.Process_Level_Activity = Constraint( model.tech_all, model.operating_period, model.operating_period, rule=Process_Level_Activity )
+model.Process_Level_Activity = Constraint( model.tech_all, model.invest_period, model.operating_period, rule=Process_Level_Activity )
 
 model.CO2_Emissions_Constraint = Constraint( model.operating_period, rule=CO2_Emissions_Constraint )
 
@@ -114,7 +115,7 @@ model.Up_Solar_Th   = Constraint( rule=Up_Solar_Th )
 ###################################################
 
 def setAnnualCosts ( per, model ):
-    return model.PeriodCost[ per ] == AnnualCost( per, model )
+	return model.PeriodCost[ per ] == AnnualCost( per, model )
 
 model.ComputeAnnualCosts = Constraint( model.operating_period, rule=setAnnualCosts)
 
