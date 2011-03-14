eabcc526aade74827d5be5053992b28f54babdbc
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2011-03-14 01:10:16 +0000

    Update model for hopefully easier use for testing.
    git-svn-id: http://svn.temoaproject.org/temoa/branches/energysystem-process@141 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- model.py
+++ model.py
@@ -1,69 +1,74 @@
 #!/usr/bin/env pyomo
 
+__all__ = [ 'create_TEMOA_model', ]
+
 from temoa_rules import *
 
-model = AbstractModel('TEMOA Entire Energy System Economic Optimization Model')
-M = model
+def create_TEMOA_model ( ):
+	M = AbstractModel('TEMOA Entire Energy System Economic Optimization Model')
 
-M.time_period     = Set()
-M.resource_tech   = Set()
-M.production_tech = Set()
-M.third           = Set()
-M.tech = M.resource_tech | M.production_tech  # '|' = union.
+	M.time_period     = Set()
+	M.resource_tech   = Set()
+	M.production_tech = Set()
+	M.third           = Set()
+	M.tech = M.resource_tech | M.production_tech  # '|' = union.
 
-M.vintage = M.time_period    # copy of time_period; used for technology vintaging
+	M.vintage = M.time_period    # copy of time_period; used for technology vintaging
 
-M.emissions_commodity = Set()
-M.physical_commodity = Set()
-M.all_outputs = Set()
-M.demand_commodity = Set()
+	M.emissions_commodity = Set()
+	M.physical_commodity = Set()
+	M.all_outputs = Set()
+	M.demand_commodity = Set()
 
-# Pyomo currently has a rather large design flaw in it's implementation of set
-# unions, where it is not possible to create a union of more than two sets in
-# a single statement.  A bug report has been filed with the Coopr devs.
-#   - 24 Feb 2011
-M.tmp_set = M.physical_commodity | M.emissions_commodity
-M.all_commidities = M.tmp_set | M.demand_commodity
-
-
-M.Efficiency     = Param(M.all_commidities, M.tech, M.time_period, M.all_commidities, default=0)
-M.Lifetime       = Param(M.tech,        M.time_period,                      default=0) # 20 years
-M.Demand         = Param(M.time_period, M.demand_commodity,                 default=0)
-M.ResourceBound  = Param(M.time_period, M.physical_commodity,               default=0)
-M.CommodityProductionCost = Param(M.time_period, M.tech, M.time_period,     default=1)
-M.CapacityFactor = Param(M.time_period, M.tech, M.time_period, M.all_commidities, default=1)
-
-# Not yet indexed by period or incorporated into the constraints
-M.EmissionsLimit = Param(M.emissions_commodity, default=0)
+	# Pyomo currently has a rather large design flaw in it's implementation of set
+	# unions, where it is not possible to create a union of more than two sets in
+	# a single statement.  A bug report has been filed with the Coopr devs.
+	#   - 24 Feb 2011
+	M.tmp_set = M.physical_commodity | M.emissions_commodity
+	M.all_commidities = M.tmp_set | M.demand_commodity
 
 
-# Variables
-#   Decision variables
-M.V_FlowIn  = Var(M.time_period, M.all_commidities, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
-M.V_FlowOut = Var(M.time_period, M.all_commidities, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
+	M.Efficiency     = Param(M.all_commidities, M.tech, M.time_period, M.all_commidities, default=0)
+	M.Lifetime       = Param(M.tech,        M.time_period,                      default=20) # 20 years
+	M.Demand         = Param(M.time_period, M.demand_commodity,                 default=0)
+	M.ResourceBound  = Param(M.time_period, M.physical_commodity,               default=0)
+	M.CommodityProductionCost = Param(M.time_period, M.tech, M.time_period,     default=1)
+	M.CapacityFactor = Param(M.time_period, M.tech, M.time_period, M.all_commidities, default=1)
 
-#   Calculated "dummy" variables
-M.V_Activity = Var(M.time_period, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
-M.V_Capacity = Var(M.time_period, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
+	# Not yet indexed by period or incorporated into the constraints
+	M.EmissionsLimit = Param(M.emissions_commodity, default=0)
 
 
-# Objective
-M.TotalCost = Objective(rule=TotalCost_rule, sense=minimize)
+	# Variables
+	#   Decision variables
+	M.V_FlowIn  = Var(M.time_period, M.all_commidities, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
+	M.V_FlowOut = Var(M.time_period, M.all_commidities, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
 
-# Constraints
+	#   Calculated "dummy" variables
+	M.V_Activity = Var(M.time_period, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
+	M.V_Capacity = Var(M.time_period, M.tech, M.vintage, M.all_commidities, domain=NonNegativeReals)
 
-#   "Bookkeeping" constraints
-M.ActivityConstraint = Constraint( M.time_period, M.tech, M.vintage, M.all_commidities, rule=ActivityConstraint_rule )
-M.CapacityConstraint = Constraint( M.time_period, M.tech, M.vintage, M.all_commidities, rule=CapacityConstraint_rule )
 
-#   Model Constraints
-#    - in driving order.  (e.g., without Demand, none of the others are
-#      very useful.
-M.DemandConstraint             = Constraint( M.time_period, M.demand_commodity,      rule=DemandConstraint_rule )
-M.ProcessBalanceConstraint     = Constraint( M.time_period, M.all_commidities, M.tech, M.vintage, M.all_commidities, rule=ProcessBalanceConstraint_rule )
-M.CommodityBalanceConstraint   = Constraint( M.time_period, M.physical_commodity,    rule=CommodityBalanceConstraint_rule )
-M.ResourceExtractionConstraint = Constraint( M.time_period, M.physical_commodity,    rule=ResourceExtractionConstraint_rule )
+	# Objective
+	M.TotalCost = Objective(rule=TotalCost_rule, sense=minimize)
 
-#   Constraints not yet updated
-#M.EmissionConstraint           = Constraint(M.emissions_commodity,            rule=EmissionConstraint_rule)
-#M.ResourceBalanceConstraint    = Constraint(M.physical_commodity,             rule=ResourceBalanceConstraint_rule)
+	# Constraints
+
+	#   "Bookkeeping" constraints
+	M.ActivityConstraint = Constraint( M.time_period, M.tech, M.vintage, M.all_commidities, rule=ActivityConstraint_rule )
+	M.CapacityConstraint = Constraint( M.time_period, M.tech, M.vintage, M.all_commidities, rule=CapacityConstraint_rule )
+
+	#   Model Constraints
+	#    - in driving order.  (e.g., without Demand, none of the others are
+	#      very useful.
+	M.DemandConstraint             = Constraint( M.time_period, M.demand_commodity,      rule=DemandConstraint_rule )
+	M.ProcessBalanceConstraint     = Constraint( M.time_period, M.all_commidities, M.tech, M.vintage, M.all_commidities, rule=ProcessBalanceConstraint_rule )
+	M.CommodityBalanceConstraint   = Constraint( M.time_period, M.physical_commodity,    rule=CommodityBalanceConstraint_rule )
+	M.ResourceExtractionConstraint = Constraint( M.time_period, M.physical_commodity,    rule=ResourceExtractionConstraint_rule )
+
+	#   Constraints not yet updated
+	#M.EmissionConstraint           = Constraint(M.emissions_commodity,            rule=EmissionConstraint_rule)
+	#M.ResourceBalanceConstraint    = Constraint(M.physical_commodity,             rule=ResourceBalanceConstraint_rule)
+	return M
+
+model = create_TEMOA_model()
