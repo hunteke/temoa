fafa139fe9cacfa8beb825afed417fb70653b828
sarat <sarat@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2011-06-07 20:17:45 +0000

    Creating a Sphinx branch for energy system
    git-svn-id: http://svn.temoaproject.org/temoa/trunk@191 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

---
+++ sphinx-energysystem/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make clean; make html
+echo "Copied new documentation to ./html sub-directory"
+cp -r _build/html html 
+
+echo -e "\n\nCopying new documentation to the temoaproject.org website\n\n"
+scp -r html/* energym@energy-modeling.org:temoaproject.org/docs 
+echo "Documentation updated on the website."
+echo -e "\n\n Please visit http://temoaproject.org/docs to check.\n\n"
