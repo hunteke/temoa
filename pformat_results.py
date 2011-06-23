a034c1ac9b20a7652ade744b3e42c62edd8e746a
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2011-06-23 16:48:09 +0000

    Correct minor bug with output numbers between -1 and 0.
    Numbers between -1 and 0 were output like this:
    
      00.12345   SomeVarOrConstraint
    
    Now, results are correct, like:
    
      -0.12345   SomeVarOrConstraint
    
    
    git-svn-id: http://svn.temoaproject.org/temoa/branches/energysystem-process@221 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- pformat_results.py
+++ pformat_results.py
@@ -29,7 +29,7 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	constraint_keys = sorted(
 	  ii
 	  for ii in Cons
-	  if 'c_' == ii[:2]            # all Coopr constraint keys being with c_
+	  if 'c_' == ii[:2]            # all Coopr constraint keys begin with c_
 	  if abs(Cons[ ii ].value) > 1e-16    # i.e. "if it's non-zero"
 	)
 
@@ -40,7 +40,7 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 		return wrapped
 	def get_dec_padding ( ObjSet ):
 		def wrapped ( key ):
-			val = ObjSet[ key ].value
+			val = abs(ObjSet[ key ].value)
 			return len(str(val - int(val)))
 		return wrapped
 
@@ -53,24 +53,28 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	# the period
 	int_padding = max(map( get_int_padding(Vars), var_keys ))
 	dec_padding = max(map( get_dec_padding(Vars), var_keys ))
-	format = "  %%%dd%%-%ds  %%s\n" % (int_padding, dec_padding)
+	format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
 		# Works out to something like "%8d%-11s  %s"
 
 	for key in var_keys:
-		int_part = int(Vars[ key ].value)
-		dec_part = str(Vars[ key ].value - int_part)[1:]
+		val = Vars[ key ].value
+		int_part = int(abs(val))
+		dec_part = str(abs(val) - int_part)[1:]
+		if val < 0: int_part = "-%d" % int_part
 		run_output.write( format % (int_part, dec_part, key) )
 
 	run_output.write( "\nBinding constraint values for '%s'\n" % instance.name)
 
 	int_padding = max(map( get_int_padding(Cons), constraint_keys ))
 	dec_padding = max(map( get_dec_padding(Cons), constraint_keys ))
-	format = "  %%%dd%%-%ds  %%s\n" % (int_padding, dec_padding)
-		# Works out to something like "%8d%-11s  %s"
+	format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
+		# Works out to something like "%8s%-11s  %s"
 
 	for key in constraint_keys:
-		int_part = int(Cons[ key ].value)
-		dec_part = str(Cons[ key ].value - int_part)[1:]
+		val = Cons[ key ].value
+		int_part = int(abs(val))
+		dec_part = str(abs(val) - int_part)[1:]
+		if val < 0: int_part = "-%d" % int_part
 		run_output.write( format % (int_part, dec_part, key[4:-1]) )
 
 	return run_output.getvalue()
