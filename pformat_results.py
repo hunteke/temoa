dde19ba1bd8c2430a2300fe4f045286bf6f1d3da
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2011-06-23 21:17:43 +0000

    Be a little more robust depending on which Coopr solver one chooses.
    git-svn-id: http://svn.temoaproject.org/temoa/branches/energysystem-process@227 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- pformat_results.py
+++ pformat_results.py
@@ -49,32 +49,40 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	run_output.write( "Objective function value: %s\n" % obj_value )
 	run_output.write( "Non-zero variable values for '%s'\n" % instance.name )
 
-	# This padding code make the display of the output values line up at
-	# the period
-	int_padding = max(map( get_int_padding(Vars), var_keys ))
-	dec_padding = max(map( get_dec_padding(Vars), var_keys ))
-	format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
-		# Works out to something like "%8d%-11s  %s"
+	if len( var_keys ) > 0:
+		# This padding code make the display of the output values line up at
+		# the period
+		int_padding = max(map( get_int_padding(Vars), var_keys ))
+		dec_padding = max(map( get_dec_padding(Vars), var_keys ))
+		format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
+			# Works out to something like "%8d%-11s  %s"
 
-	for key in var_keys:
-		val = Vars[ key ].value
-		int_part = int(abs(val))
-		dec_part = str(abs(val) - int_part)[1:]
-		if val < 0: int_part = "-%d" % int_part
-		run_output.write( format % (int_part, dec_part, key) )
+		for key in var_keys:
+			val = Vars[ key ].value
+			int_part = int(abs(val))
+			dec_part = str(abs(val) - int_part)[1:]
+			if val < 0: int_part = "-%d" % int_part
+			run_output.write( format % (int_part, dec_part, key) )
 
-	run_output.write( "\nBinding constraint values for '%s'\n" % instance.name)
+	if len( constraint_keys ) == 0:
+		# Since not all Coopr solvers give constraint results, must check
+		msg = '\nSelected Coopr solver plugin does not give constraint '        \
+		   'information.\n'
+		run_output.write( msg )
+	else:
+		msg = "\nBinding constraint values for '%s'\n"
+		run_output.write( msg % instance.name)
 
-	int_padding = max(map( get_int_padding(Cons), constraint_keys ))
-	dec_padding = max(map( get_dec_padding(Cons), constraint_keys ))
-	format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
-		# Works out to something like "%8s%-11s  %s"
+		int_padding = max(map( get_int_padding(Cons), constraint_keys ))
+		dec_padding = max(map( get_dec_padding(Cons), constraint_keys ))
+		format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
+			# Works out to something like "%8s%-11s  %s"
 
-	for key in constraint_keys:
-		val = Cons[ key ].value
-		int_part = int(abs(val))
-		dec_part = str(abs(val) - int_part)[1:]
-		if val < 0: int_part = "-%d" % int_part
-		run_output.write( format % (int_part, dec_part, key[4:-1]) )
+		for key in constraint_keys:
+			val = Cons[ key ].value
+			int_part = int(abs(val))
+			dec_part = str(abs(val) - int_part)[1:]
+			if val < 0: int_part = "-%d" % int_part
+			run_output.write( format % (int_part, dec_part, key[4:-1]) )
 
 	return run_output.getvalue()
