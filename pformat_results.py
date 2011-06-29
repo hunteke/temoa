0f8cdff9d09a555aea62b55d287d1feab33fbc3b
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2011-06-29 03:18:38 +0000

    Prepare this library for Coopr 3.0
    With this commit, this particular library is presently compatible
    with Coopr v3.0 and pre v3.0.  We'll see if that holds ...
    
    
    git-svn-id: http://svn.temoaproject.org/temoa/branches/energysystem-process@259 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- pformat_results.py
+++ pformat_results.py
@@ -15,7 +15,7 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	if str(soln.Status) not in optimal_solutions:
 		return "No solution found."
 
-	objs = pyomo_instance.objectives()
+	objs = instance.objectives()
 	if len( objs ) > 1:
 		msg = '\nWarning: More than one objective.  Using first objective.\n'
 		SE.write( msg )
@@ -28,15 +28,38 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	Vars = soln.Variable
 	Cons = soln.Constraint
 
-	var_keys = sorted(
-	  (ii.split('[')[0], ii)
+	try:
+		# This /should/ work with Coopr v3.0
 
-	  for ii in Vars
-	  if abs(Vars[ ii ].value) > 1e-15   # i.e. "if it's non-zero"
-	)
+		# Sanitize name mapping so we have a consistent interface later
+		tmpv, tmpc = instance._label_var_map, instance._label_constraint_map
+		var_map = dict({ key : tmpv[ key ].name for key in tmpv })
+		con_map = dict({ key : tmpc[ key ].name for key in tmpc })
 
-	constraint_keys = sorted(
-	  (ii.split('[')[0][4:], ii)   # [4:] removes the c_[uel]_ part of name
+		var_keys = sorted(
+		  (ii.split('(')[0], ii, Vars[ ii ]['Value'])
+
+		  for ii in Vars
+		  if abs(Vars[ ii ]['Value']) > 1e-15   # i.e. "if it's non-zero"
+		)
+
+	except AttributeError:
+		# We are using Coopr prior v3.0
+
+		# Sanitize name mapping so we have a consistent interface later
+		var_map = dict({ key : key for key in instance.variables().keys() })
+		con_map = dict({ key : key for key in instance.constraints().keys() })
+
+		var_keys = sorted(
+		  (ii.split('[')[0], ii, Vars[ ii ]['Value'])
+
+		  for ii in Vars
+		  if abs(Vars[ ii ]['Value']) > 1e-15   # i.e. "if it's non-zero"
+		)
+
+	con_keys = sorted(
+	  (ii.split('[')[0][4:], ii[4:-1], Cons[ ii ]['Value'])
+	    # [4:-1] removes the c_[uel]_ part of name
 
 	  for ii in Cons
 	  if 'c_' == ii[:2]            # all Coopr constraint keys begin with c_
@@ -44,19 +67,15 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	)
 
 	# remove the no-longer-necessary sorting key.
-	var_keys        = [ var for key, var in var_keys ]
-	constraint_keys = [ con for key, con in constraint_keys ]
+	var_info = [ (var_map[ var ], val) for key, var, val in var_keys ]
+	con_info = [ (con_map[ con ], val) for key, con, val in con_keys ]
 
-	def get_int_padding ( ObjSet ):
-		def wrapped ( key ):
-			val = ObjSet[ key ].value
-			return len(str(int(val)))
-		return wrapped
-	def get_dec_padding ( ObjSet ):
-		def wrapped ( key ):
-			val = abs(ObjSet[ key ].value)
-			return len(str(val - int(val)))
-		return wrapped
+	def get_int_padding ( obj ):
+		val = obj[ 1 ]         # obj is 2-tuple, defined within var_info
+		return len(str(int(val)))
+	def get_dec_padding ( obj ):
+		val = abs(obj[ 1 ])    # obj is 2-tuple defined within con_info
+		return len(str(val - int(val)))
 
 	run_output = StringIO()
 
@@ -68,19 +87,18 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 	if len( var_keys ) > 0:
 		# This padding code is what makes the display of the output values
 		# line up on the decimal point.
-		int_padding = max(map( get_int_padding(Vars), var_keys ))
-		dec_padding = max(map( get_dec_padding(Vars), var_keys ))
+		int_padding = max(map( get_int_padding, var_info ))
+		dec_padding = max(map( get_dec_padding, var_info ))
 		format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
 			# Works out to something like "%8d%-11s  %s"
 
-		for key in var_keys:
-			val = Vars[ key ].value
+		for key, val in var_info:
 			int_part = int(abs(val))
-			dec_part = str(abs(val) - int_part)[1:]
+			dec_part = str(abs(val) - int_part)[1:]  # remove (negative and) 0
 			if val < 0: int_part = "-%d" % int_part
 			run_output.write( format % (int_part, dec_part, key) )
 
-	if 0 == len( constraint_keys ):
+	if 0 == len( con_info ):
 		# Since not all Coopr solvers give constraint results, must check
 		msg = '\nSelected Coopr solver plugin does not give constraint data.\n'
 		run_output.write( msg )
@@ -88,16 +106,18 @@ def pformat_results ( pyomo_instance, pyomo_result ):
 		msg = '\nBinding constraint values:\n'
 		run_output.write( msg )
 
-		int_padding = max(map( get_int_padding(Cons), constraint_keys ))
-		dec_padding = max(map( get_dec_padding(Cons), constraint_keys ))
+		int_padding = max(map( get_int_padding, con_info ))
+		dec_padding = max(map( get_dec_padding, con_info ))
 		format = "  %%%ds%%-%ds  %%s\n" % (int_padding, dec_padding)
 			# Works out to something like "%8s%-11s  %s"
 
-		for key in constraint_keys:
-			val = Cons[ key ].value
+		for key, val in con_info:
 			int_part = int(abs(val))
 			dec_part = str(abs(val) - int_part)[1:]
 			if val < 0: int_part = "-%d" % int_part
-			run_output.write( format % (int_part, dec_part, key[4:-1]) )
+			run_output.write( format % (int_part, dec_part, key) )
 
-	return run_output.getvalue()
+	data = run_output.getvalue()
+
+	  # remove the new 3.0 format's superfluous apostrophes
+	return data.replace("'",'')
