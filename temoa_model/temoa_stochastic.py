661240ad05a1ae1624f57cb1cea4fc3d84b90a34
kevin <kevin@3acdfd15-2c5e-48eb-ba72-2386a29e86c2>
2012-02-24 13:46:29 +0000

    NFC: some mild changes, and a comment TODO added.
    git-svn-id: http://svn.temoaproject.org/temoa/branches/energysystem-process-Coopr3@448 3acdfd15-2c5e-48eb-ba72-2386a29e86c2

--- temoa_model/temoa_stochastic.py
+++ temoa_model/temoa_stochastic.py
@@ -4,37 +4,62 @@ from temoa_lib import *
 from temoa_model import *
 
 def StochasticPointObjective_rule ( M, A_period ):
-	l_cost = 0
 	"""\
-Objective function.
+Stochastic objective function.
 
-This function is currently a simple summation of all items in V_FlowOut multiplied by CommunityProductionCost.  For the time being (i.e. during development), this is intended to make development and debugging simpler.
+TODO: update with LaTeX version of equation.
 	"""
-	l_invest_indices = M.CostInvest.keys()
-	l_fixed_indices  = M.CostFixed.keys()
-	l_marg_indices   = M.CostMarginal.keys()
+	l_loan_period_fraction_indices = M.LoanLifeFrac.keys()
+	l_tech_period_fraction_indices = M.TechLifeFrac.keys()
 
 	l_loan_costs = sum(
-	    M.V_Capacity[l_tech, l_vin] * M.PeriodRate[ A_period ]
-	  * M.CostInvest[l_tech, l_vin]
-	  * M.LoanAnnualize[l_tech, l_vin]
+	    M.V_CapacityInvest[l_tech, l_vin]
+	  * value(
+	      M.PeriodRate[ A_period ].value
+	    * M.CostInvest[l_tech, l_vin].value
+	    * M.LoanAnnualize[l_tech, l_vin].value
+	  )
 
-	  for l_tech in M.tech_all
-	  for l_vin in ProcessVintages( A_period, l_tech )
+	  for l_tech, l_vin in M.CostInvest.keys()
+	  if (A_period, l_tech, l_vin) not in l_loan_period_fraction_indices
 	  if loanIsActive( A_period, l_tech, l_vin )
-	  if (l_tech, l_vin) in l_invest_indices
-	  if value(M.CostInvest[l_tech, l_vin])
+	) + sum(
+	    M.V_CapacityInvest[l_tech, l_vin]
+	  * value(
+	      M.CostInvest[l_tech, l_vin].value
+	    * M.LoanAnnualize[l_tech, l_vin].value
+	  )
+	  * sum(
+	      (1 + M.GlobalDiscountRate) ** (M.time_optimize.first() - l_per - y)
+	      for y in range( 0, M.PeriodLength[ l_per ] * M.LoanLifeFrac[l_per, l_tech, l_vin])
+	    )
+
+	  for l_per, l_tech, l_vin in l_loan_period_fraction_indices
+	  if l_per == A_period
 	)
 
 	l_fixed_costs = sum(
 	    M.V_Capacity[l_tech, l_vin]
-	  * M.CostFixed[A_period, l_tech, l_vin]
-	  * M.PeriodRate[ A_period ]
+	  * value(
+	      M.CostFixed[A_period, l_tech, l_vin].value
+	    * M.PeriodRate[ A_period ].value
+	  )
 
-	  for l_tech in M.tech_all
-	  for l_vin in ProcessVintages( A_period, l_tech )
-	  if (A_period, l_tech, l_vin) in l_fixed_indices
-	  if value(M.CostFixed[A_period, l_tech, l_vin])
+	  for l_per, l_tech, l_vin in M.CostFixed.keys()
+	  if l_per == A_period
+	  if (l_per, l_tech, l_vin) not in l_tech_period_fraction_indices
+	) + sum(
+	    M.V_CapacityInvest[l_tech, l_vin]
+	  * M.CostInvest[l_tech, l_vin].value
+	  * M.LoanAnnualize[l_tech, l_vin].value
+	  * sum(
+	      (1 + M.GlobalDiscountRate) ** (M.time_optimize.first() - l_per - y)
+	      for y in range( 0, M.PeriodLength[ l_per ] * M.TechLifeFrac[l_per, l_tech, l_vin])
+	    )
+
+	  for l_per, l_tech, l_vin in l_tech_period_fraction_indices
+	  if l_per == A_period
+	  if (l_per, l_tech, l_vin) in M.CostFixed.keys()
 	)
 
 	l_marg_costs = sum(
@@ -42,13 +67,14 @@ This function is currently a simple summation of all items in V_FlowOut multipli
 	  * M.PeriodRate[ A_period ]
 	  * M.CostMarginal[A_period, l_tech, l_vin]
 
-	  for l_tech in M.tech_all
-	  for l_vin in ProcessVintages( A_period, l_tech )
-	  if (A_period, l_tech, l_vin) in l_marg_indices
-	  if value(M.CostMarginal[A_period, l_tech, l_vin])
+	  for l_per, l_tech, l_vin in M.CostMarginal.keys()
+	  if l_per == A_period
+	  if (l_per, l_tech, l_vin) not in l_tech_period_fraction_indices
 	  for l_season in M.time_season
 	  for l_time_of_day in M.time_of_day
 	)
+
+	l_cost = (l_loan_costs + l_fixed_costs + l_marg_costs)
 
 	expr = (M.StochasticPointCost[ A_period ] == l_cost)
 	return expr
@@ -61,4 +87,4 @@ M = model = temoa_create_model( 'TEMOA Stochastic' )
 M.StochasticPointCost = Var( M.time_optimize, within=NonNegativeReals )
 M.StochasticPointCostConstraint = Constraint( M.time_optimize, rule=StochasticPointObjective_rule )
 
-M.Total_Cost = Objective( rule=Objective_rule, sense=minimize )
+M.TotalCost = Objective( rule=Objective_rule, sense=minimize )
