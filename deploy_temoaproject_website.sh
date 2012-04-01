52f74c4446d5e81ee253d6dfce7099ffe4dec2dc
Kevin Hunter <hunteke@earlham.edu>
2012-04-01 15:46:39 -0400

    Minor cleanup, and add some analytics code.
    Also, convert from &ldquo; to <q>..</q>

--- deploy_temoaproject_website.sh
+++ deploy_temoaproject_website.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 UPDDIR='.temoaproject.org-updating'
-UPDPKG='.temoaproject.org-updating.tgz'
+UPDPKG='.temoaproject.org-updating.tbz'
 DELDIR='.temoaproject.org-deleting'
 WEBDIR='temoaproject.org'
 
-tar -cf - * .htaccess | gzip --best | ssh temoaproject.org "cat > .temoaproject.org-updating.tgz" && \
+tar -cf - * .htaccess | bzip2 --best | ssh temoaproject.org "cat > '$UPDPKG'" && \
   ssh temoaproject.org "rm -rf '$UPDDIR' && mkdir '$UPDDIR' && (cd '$UPDDIR'; tar -xf ../'$UPDPKG') && \
   mv '$WEBDIR' '$DELDIR' && mv '$UPDDIR' '$WEBDIR' && rm -rf '$DELDIR' '$UPDPKG'"
