f0ba1c0aedaf5d31c6c7729829f9be2e7952538d
Kevin Hunter <hunteke@earlham.edu>
2012-11-16 23:06:09 -0500

    Temoa is now licensed under the AGPLv3+.
    This means that it is licensed to any entity under the terms of the AGPL
    version 3, or any later version.

--- temoa_model/temoa_stochastic.py
+++ temoa_model/temoa_stochastic.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env coopr_python
 
+"""
+Temoa - Tools for Energy Model Optimization and Analysis
+  linear optimization; least cost; dynamic system visualization
+
+Copyright (C) 2011-2012  Kevin Hunter, Joseph DeCarolis
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+Developers of this script will check out a complete copy of the GNU Affero
+General Public License in the file COPYING.txt.  Users uncompressing this from
+an archive may not have received this license file.  If not, see
+<http://www.gnu.org/licenses/>.
+"""
+
 from temoa_lib import *
 from temoa_model import *
 
