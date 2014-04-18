a21d773f3e5ad5e8cbd9145daee4f11fc0df401c
Kevin Hunter <hunteke@earlham.edu>
2014-04-18 00:07:27 -0400

    Analysis creation test.
    Thanks to this test, at least one real UI bug is fixed (new analysis
    data showing up without having to reload the page), as well as some more
    robust data validation.
    
    With this test, we have at least one set of functional tests from which
    others can scaffold.

--- process_interface/tests/test_views.py
+++ process_interface/tests/test_views.py
@@ -202,17 +202,203 @@ class ViewAnalysisTest ( TestCase ):
 			self.assertEqual( len(c.session.items()), 0 )
 
 
-	def test_create_analysis ( self ):
+	def test_analysis_creation ( self ):
 		"""
-		Tests that user can create analyses.
+		Ensure that:
+		 1. a user can create a analysis
+		 2. new analysis details are returned in response
+		 3. new analysis is listed by analysis_list
+		 4. a second analysis by the same name fails
+		 5. a second analysis by a different name succeeds
+		 6. analysis creation fails if no vintages are passed
+		 7. analysis name removes any newline characters
+		 8. analysis description stripped, and contains only unix newlines (\n)
 		"""
+		import json
 
-		a = Analysis.objects.create(
-		  user        = self.user,
-		  name        = 'Some Analysis Name',
-		  description = 'Some analysis description',
-		  period_0    = 1,
-		  global_discount_rate = 0.01,
-		)
+		c = self.client
 
+		analysis_create_url = reverse('process_interface:analysis_create')
+		analysis_list_url   = reverse('process_interface:analysis_list')
+		analysis_1 = {
+		  'name'        : 'Test Analysis 1',
+		  'description' : 'Some really awesome description',
+		  'period_0'    : '0',
+		  'global_discount_rate' : '0',
+		  'vintages'    : '-5, -4,-3,-2,5,3,2,1,0'
+		}
+		analysis_2 = {
+		  'name'        : 'Test Analysis 2',
+		  'description' : 'Some other description',
+		  'period_0'    : '0',
+		  'global_discount_rate' : '0',
+		  'vintages'    : '2010, 2013, 2011'
+		}
+		no_vintages = {
+		  'name'        : 'No Vintages ... Should fail',
+		  'description' : 'Another description',
+		  'period_0'    : '0',
+		  'global_discount_rate' : '0.5'
+		}
+		newlines_in_name = {
+		  'name'        : '\n\rAre\0\t\r\n control\n characters\r removed?\n ',
+		  'description' : 'Some description',
+		  'period_0'    : '15',
+		  'global_discount_rate' : '0.5',
+		  'vintages'    : '15, 17, 19'
+		}
+		nonunix_newlines_in_description = {
+		  'name'        : 'Some Analysis name',
+		  'description' : '\r\nSome \r\n descripti\ton \n \r\r \t ',
+		  'period_0'    : '15',
+		  'global_discount_rate' : '0.5',
+		  'vintages'    : '15, 17, 19'
+		}
+
+		# Part 1: Can we successfully create a new analysis?
+		res = c.post( analysis_create_url, analysis_1 )
+
+		self.assertEqual( res.status_code, 201 )
+		self.assertGreater( len(res.content), 0 )
+
+		# Part 2: Is new analysis data returned as a JSON object in response?
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'Newly created analysis data not JSON encoded: {}'
+			self.fail( msg.format( res.content ))
+
+		self.assertIn( 'id', ret_data )
+		self.assertTrue( isinstance( ret_data['id'], int ))
+		self.assertGreater( ret_data['id'], 0 )
+
+		analysis = Analysis.objects.get( id=ret_data['id'] )
+
+		# this will need to be updated for Python3, due to unicode vs bytes
+		# but from the department of "Deal with that later" ...
+		self.assertEqual( ret_data['username'], self.user.username )
+		self.assertEqual( ret_data['name'], analysis.name )
+		self.assertEqual( ret_data['description'], analysis.description )
+		self.assertEqual( ret_data['period_0'], analysis.period_0 )
+		self.assertAlmostEqual( ret_data['global_discount_rate'], analysis.global_discount_rate )
+
+		vintages = Vintage.objects.filter( analysis=analysis )
+		vintages = sorted( v.vintage for v in vintages )
+		vintages = u', '.join( unicode(i) for i in vintages)
+		self.assertEqual( ret_data['vintages'], vintages )
+
+		# Part 3: ensure TemoaDB has stored new analysis for others to view
+		res = c.get( analysis_list_url )
+
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'Analysis listing is not valid JSON: {}'
+			self.fail( msg.format( res.content ))
+
+		self.assertIn( 'data', ret_data )
+		ret_data = ret_data['data']
+		ids = set( a_data['id'] for a_data in ret_data )
+		names = set( a_data['name'] for a_data in ret_data )
+
+		self.assertIn( analysis.pk, ids )
+		self.assertIn( analysis.name, names )
+
+		# Part 4: Ensure that attempting to create a second analysis by the
+		#         same name fails, and returns HTTP Status code 422
+		res = c.post( analysis_create_url, analysis_1 )
+
+		self.assertEqual( res.status_code, 422 )
+		self.assertEqual( res.reason_phrase, u'UNPROCESSABLE ENTITY' )
+		self.assertIn( 'An analysis by this name already exists.', res.content )
+
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'HttpResponse (error) object content is not valid JSON: {}'
+			self.fail( msg.format( res.content ))
+
+		# Part 5: Ensure that attempting to create a second analysis by a
+		#         different name succeeds.
+		res = c.post( analysis_create_url, analysis_2 )
+
+		self.assertEqual( res.status_code, 201 )
+		self.assertEqual( res.reason_phrase, u'CREATED' )
+		self.assertGreater( len(res.content), 0 )
+
+		# And is the second analysis data returned as a JSON object in response?
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'Newly created analysis data not JSON encoded: {}'
+			self.fail( msg.format( res.content ))
+
+		self.assertIn( 'id', ret_data )
+		self.assertTrue( isinstance( ret_data['id'], int ))
+		self.assertGreater( ret_data['id'], 0 )
+
+		analysis = Analysis.objects.get( id=ret_data['id'] )
+
+		# this will need to be updated for Python3, due to unicode vs bytes
+		# but from the department of "Deal with that later" ...
+		self.assertEqual( ret_data['username'], self.user.username )
+		self.assertEqual( ret_data['name'], analysis.name )
+		self.assertEqual( ret_data['description'], analysis.description )
+		self.assertEqual( ret_data['period_0'], analysis.period_0 )
+		self.assertAlmostEqual( ret_data['global_discount_rate'], analysis.global_discount_rate )
+
+		vintages = Vintage.objects.filter( analysis=analysis )
+		vintages = sorted( v.vintage for v in vintages )
+		vintages = u', '.join( unicode(i) for i in vintages)
+		self.assertEqual( ret_data['vintages'], vintages )
+
+		# Part 6: Ensure that having some vintages is enforced
+		res = c.post( analysis_create_url, no_vintages )
+
+		self.assertEqual( res.status_code, 422 )
+		self.assertEqual( res.reason_phrase, u'UNPROCESSABLE ENTITY' )
+		self.assertIn( 'vintages', res.content )
+		self.assertIn( 'required', res.content )
+
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'HttpResponse (error) object content is not valid JSON: {}'
+			self.fail( msg.format( res.content ))
+
+		# Part 7: Ensure name with newline/tab characters gets cleaned
+		res = c.post( analysis_create_url, newlines_in_name )
+
+		self.assertEqual( res.status_code, 201 )
+		self.assertEqual( res.reason_phrase, u'CREATED' )
+
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'HttpResponse (new analysis) object content is not valid JSON: {}'
+			self.fail( msg.format( res.content ))
+
+		self.assertNotIn( u'\n', ret_data['name'] )
+		self.assertNotIn( u'\r', ret_data['name'] )
+		self.assertNotIn( u'\t', ret_data['name'] )
+		self.assertNotIn( u'\0', ret_data['name'] )
+		self.assertFalse( ret_data['name'].startswith(' ') )
+		self.assertFalse( ret_data['name'].endswith(' ') )
+
+		# Part 8: Ensure description is strip()ed and has '\r' characters del'd.
+		res = c.post( analysis_create_url, nonunix_newlines_in_description )
+
+		self.assertEqual( res.status_code, 201 )
+		self.assertEqual( res.reason_phrase, u'CREATED' )
+
+		try:
+			ret_data = json.loads( res.content )
+		except ValueError as e:
+			msg = 'HttpResponse (new analysis) object content is not valid JSON: {}'
+			self.fail( msg.format( res.content ))
+
+		self.assertNotIn( u'\r', ret_data['description'] )
+		self.assertNotIn( u'\0', ret_data['description'] )
+		self.assertFalse( ret_data['description'].startswith(' ') )
+		self.assertFalse( ret_data['description'].endswith(' ') )
 
