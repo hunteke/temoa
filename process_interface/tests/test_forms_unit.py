91f70a5bfc7c08186b35022e5d618e6177a72cf1
Kevin Hunter <hunteke@earlham.edu>
2014-04-23 02:16:21 -0400

    f is shorter than form.  For the lazy.  Which I am.

--- process_interface/tests/test_forms_unit.py
+++ process_interface/tests/test_forms_unit.py
@@ -9,57 +9,57 @@ from process_interface.forms import (
 class TestLoginForm ( TestCase ):
 
 	def test_username_required ( self ):
-		form = LoginForm({ b'password': b'asdf' })
+		f = LoginForm({ b'password': b'asdf' })
 
-		self.assertFalse( form.is_valid() )
-		self.assertIn( 'username', form.errors )
-		self.assertIn( 'is required', str(form.errors['username']) )
+		self.assertFalse( f.is_valid() )
+		self.assertIn( 'username', f.errors )
+		self.assertIn( 'is required', str(f.errors['username']) )
 
 
 	def test_password_required ( self ):
-		form = LoginForm({ b'username': b'asdf' })
+		f = LoginForm({ b'username': b'asdf' })
 
-		self.assertFalse( form.is_valid() )
-		self.assertIn( 'password', form.errors )
-		self.assertIn( 'is required', str(form.errors['password']) )
+		self.assertFalse( f.is_valid() )
+		self.assertIn( 'password', f.errors )
+		self.assertIn( 'is required', str(f.errors['password']) )
 
 
 	def test_password_is_not_empty ( self ):
-		form = LoginForm({ b'username': b'asdf', b'password': b'' })
+		f = LoginForm({ b'username': b'asdf', b'password': b'' })
 
-		self.assertFalse( form.is_valid() )
-		self.assertIn( 'password', form.errors )
-		self.assertIn( 'is required', str(form.errors['password']) )
+		self.assertFalse( f.is_valid() )
+		self.assertIn( 'password', f.errors )
+		self.assertIn( 'is required', str(f.errors['password']) )
 
 
 	def test_max_length_is_254 ( self ):
-		form = LoginForm({ b'username': b'a' * 255, b'password': b'asdf' })
+		f = LoginForm({ b'username': b'a' * 255, b'password': b'asdf' })
 
-		self.assertFalse( form.is_valid() )
-		self.assertIn( 'username', form.errors )
-		self.assertIn( 'has at most 254 cha', str(form.errors['username']) )
+		self.assertFalse( f.is_valid() )
+		self.assertIn( 'username', f.errors )
+		self.assertIn( 'has at most 254 cha', str(f.errors['username']) )
 
 
 	def test_valid_userpass ( self ):
-		form = LoginForm({ b'username': b'UserName', b'password': b'UserPass' })
+		f = LoginForm({ b'username': b'UserName', b'password': b'UserPass' })
 
-		self.assertTrue( form.is_valid() )
+		self.assertTrue( f.is_valid() )
 
 
 	def test_common_control_characters_invalid_in_username ( self ):
-		form = LoginForm({ b'username': b'a\n', b'password': b'a' })
+		f = LoginForm({ b'username': b'a\n', b'password': b'a' })
 
-		self.assertFalse( form.is_valid() )
-		self.assertIn( 'username', form.errors )
+		self.assertFalse( f.is_valid() )
+		self.assertIn( 'username', f.errors )
 		self.assertIn( 'characters on the keyboard',
-		  str(form.errors['username']) )
+		  str(f.errors['username']) )
 
 
 	def test_nullbyte_is_invalid_in_password ( self ):
-		form = LoginForm({ b'username': b'a', b'password': b'a\0a' })
+		f = LoginForm({ b'username': b'a', b'password': b'a\0a' })
 
-		self.assertFalse( form.is_valid() )
-		self.assertIn( 'password', form.errors )
+		self.assertFalse( f.is_valid() )
+		self.assertIn( 'password', f.errors )
 		self.assertIn( 'characters on the keyboard',
-		  str(form.errors['password']) )
+		  str(f.errors['password']) )
 
