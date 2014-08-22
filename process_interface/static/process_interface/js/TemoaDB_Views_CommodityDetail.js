9ddeb074ed99184830ebf088e1b2d3b449899e12
Kevin Hunter Kesling <hunteke@earlham.edu>
2014-08-22 14:30:49 -0400

    Move two more controls to their own files.
    CommodityDetail and CommodityLists.

---
+++ process_interface/static/process_interface/js/TemoaDB_Views_CommodityDetail.js
@@ -0,0 +1,134 @@
+;(function () {
+
+"use strict";  // ECMA v5 pragma, similar to Perl's functionality.
+  // FYI: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
+
+if ( !('Temoa' in window) ) {
+	var msg = 'Error: The Temoa namespace is not defined.  Cannot continue.';
+	msg += '\n\nThis likely means that one or more TemoaUI files did not ';
+	msg += 'download from the server, implying at least a transient network ';
+	msg += 'failure.  In most cases, <strong>a simple reload of this page will ';
+	msg += 'fix the issue</strong>.  If not, however, please share the problem ';
+	msg += '-- and importantly, how to create it -- on the Temoa Project forum.';
+	console.error( msg )
+	throw msg;
+}
+
+
+Temoa.canControl.CommodityDetail = can.Control('CommodityDetail', {
+	defaults: {
+			view: Temoa.C.ROOT_URL + '/client_template/analysis_commodity_detail.ejs'
+		}
+	},{
+	init: function ( $el, options ) {
+		var view = options.view;
+		if ( Temoa.C.DEBUG )
+			view += '?_=' + new Date().getTime();
+
+		this.commodity = options.commodity;
+
+		var view_opts = {
+			username:   options.username || null,
+			analysis:   options.analysis,
+			commodity:  options.commodity
+		};
+
+		$el.append( can.view( view, view_opts ));
+	},
+	save: function ( $el ) {
+		var errors = {};
+		var $form  = $el.closest( 'form' );
+		var inputs = $form.find(':input');
+		var data   = can.deparam( $form.serialize() );
+
+		Temoa.fn.disable( inputs );
+		$form.find('.error').empty();  // remove any previous errors
+
+		data.name = $.trim( data.name );
+		if ( data.name.length < 1 ) {
+			var msg = 'Please provide a name this commodity.';
+			errors[ 'name' ] = [msg];
+		}
+		else if ( ! data.name.match( /^[A-z_]\w*$/ ) ) {
+			var msg = 'Please only include alphanumeric characters, and begin ';
+			msg += 'with a letter.  Note that this is used as an identifier in ';
+			msg += 'the data file, so spaces are not valid.  If you understand ';
+			msg += '"regular expressions", then follow the form ';
+			msg += "'<code>[A-z_]\\w*<code>'.";
+			errors[ 'name' ] = [msg];
+		}
+
+		if ( Object.keys( errors ).length > 0 ) {
+			// client-side checking for user convenience.  The server will check
+			// for itself, of course.
+			Temoa.fn.enable( inputs );
+			Temoa.fn.displayErrors( $form, errors );
+			return;
+		}
+
+		var model = $el.closest('.commodity').data('commodity');
+
+		// Use a 'saver' object to simulate atomicity. Only if there is success
+		// do we update the model the UI is actually using.  Meanwhile, remove
+		// the id before destroying the saver object (after we're done with it)
+		// so as not to remove the object in the database.
+		var saver = new model.constructor( model.attr() );
+		saver.real_model = model;  // because {created} event recieves saver obj.
+
+		saver.attr( data ).save(
+			function ( updated_model ) {
+				Temoa.fn.enable( inputs );
+				model.attr( updated_model.attr() );  // "atomically" update
+
+				saver.real_model = null;
+				saver.attr( {id: null} ).destroy();  // don't delete in DB
+
+				Temoa.fn.showStatus('Saved!', 'info' );
+			}, function ( xhr ) {
+				Temoa.fn.enable( inputs );
+				saver.attr({id: null}).destroy();  // don't delete in DB
+
+				if ( xhr && xhr.responseJSON ) {
+					Temoa.fn.displayErrors( $form, xhr.responseJSON );
+				}
+		});
+	},
+	'[name="CommoditiesCancel"] click': function ( $el, ev ) {
+		var $block = $el.closest('.commodity');
+		var c = $block.data('process');
+		$block.find('[name="name"]').val( c.attr('name') || '' );
+	},
+	'[name="CommoditiesUpdate"] click': function ( $el, ev ) {
+		this.save( $el );
+	},
+	'[name="CommoditiesCreate"] click': function ( $el, ev ) {
+		this.save( $el );
+	},
+	'input keyup': function ( $el, ev ) {
+		if ( 13 === ev.keyCode ) { // 13 == enter
+			this.save( $(ev.target) );
+		}
+	},
+	'{AnalysisCommodityDemand} destroyed': function ( Model, ev, commodity ) {
+		if ( this.commodity === commodity ) {
+			var $div = this.element.find( '#CommodityForm_' + commodity.id );
+			$div.remove();
+		}
+	},
+	'{AnalysisCommodityEmission} destroyed': function ( Model, ev, commodity ) {
+		if ( this.commodity === commodity ) {
+			var $div = this.element.find( '#CommodityForm_' + commodity.id );
+			$div.remove();
+		}
+	},
+	'{AnalysisCommodityPhysical} destroyed': function ( Model, ev, commodity ) {
+		if ( this.commodity === commodity ) {
+			var $div = this.element.find( '#CommodityForm_' + commodity.id );
+			$div.remove();
+		}
+	}
+});
+
+})();
+
+console.log( 'TemoaDB CommodityDetail View loaded: ' + Date() );
---
+++ process_interface/static/process_interface/js/TemoaDB_Views_CommodityLists.js
@@ -0,0 +1,177 @@
+;(function () {
+
+"use strict";  // ECMA v5 pragma, similar to Perl's functionality.
+  // FYI: http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
+
+if ( !('Temoa' in window) ) {
+	var msg = 'Error: The Temoa namespace is not defined.  Cannot continue.';
+	msg += '\n\nThis likely means that one or more TemoaUI files did not ';
+	msg += 'download from the server, implying at least a transient network ';
+	msg += 'failure.  In most cases, <strong>a simple reload of this page will ';
+	msg += 'fix the issue</strong>.  If not, however, please share the problem ';
+	msg += '-- and importantly, how to create it -- on the Temoa Project forum.';
+	console.error( msg )
+	throw msg;
+}
+
+
+Temoa.canControl.CommodityLists = can.Control('AnalysisCommodityLists', {
+	defaults: {
+			view: Temoa.C.ROOT_URL + '/client_template/analysis_commodities.ejs'
+		}
+	},{
+	init: function ( $el, options ) {
+		var view = options.view;
+		if ( Temoa.C.DEBUG )
+			view += '?_=' + new Date().getTime();
+
+		this.analysis = options.analysis;
+		var analysis = this.analysis;  // needed for closure, below
+
+		$el.html( can.view( view, {
+			analysis: analysis,
+			username: Temoa.fn.getCookie().username || null,
+		}));
+
+		$('#AnalysisCommoditiesCloseButton').click( function ( ) {
+			$('#ShowHideCommodities').click();
+		});
+
+		var username = Temoa.fn.getCookie().username;
+		if ( ! (username && username === analysis.username ) )
+			return;
+
+		function select_start () {
+			var $others = $(this).closest( 'form' ).find( 'tbody' ).not( this );
+			$others.find('.ui-selected').removeClass( 'ui-selected' );
+			$('#commodity_detail').fadeOut();
+		}
+		function select_stop () {
+			var $info = $('#commodity_detail');
+
+			var $sel = $( this ).find( 'tr.ui-selected' );
+			var coms = new Array();
+			for ( var i = 0; i < $sel.length; ++i ) {
+				coms.push( $($sel[i]).data().commodity );
+			}
+			if ( ! (coms.length > 0) ) {
+				return;
+			}
+			var username = Temoa.fn.getCookie().username || null;
+
+			function createCommodityDetail ( toCreate ) {
+				new CommodityDetail( $info, {
+					analysis:  analysis,
+					commodity: toCreate.shift(),
+					username:  username
+				});
+
+				if ( toCreate.length > 0 ) {
+					setTimeout( function () {
+						createCommodityDetail( toCreate );
+					}, 50 );  // 50 = something tiny; i.e., "reluinquish thread"
+				}
+			}
+
+			$info.fadeOut( 'fast', function ( ) {
+				// these steps are crucial: the Detail Controls are bound to the
+				// $info element, so it must be removed for GC.  We then recreate
+				// it, and accordingly set the closure variable ($info)
+				var $newInfo = $('<div>', {id: 'commodity_detail'} );
+				$newInfo.css('display', 'none');
+				$info.replaceWith( $newInfo );
+				$info = $newInfo.fadeIn();
+
+				createCommodityDetail( coms );
+			});
+		};
+
+		var $tables = $el.find('.items');
+
+		var $tDemand   = $tables.filter('.demand').find('tbody');
+		var $tEmission = $tables.filter('.emission').find('tbody');
+		var $tPhysical = $tables.filter('.physical').find('tbody');
+
+		$tDemand.selectable( {} );
+		$tDemand.on( 'selectablestart', select_start );
+		$tDemand.on( 'selectablestop', select_stop );
+		$tEmission.selectable( {} );
+		$tEmission.on( 'selectablestart', select_start );
+		$tEmission.on( 'selectablestop', select_stop );
+		$tPhysical.selectable( {} );
+		$tPhysical.on( 'selectablestart', select_start );
+		$tPhysical.on( 'selectablestop', select_stop );
+
+	},
+	create: function ( $el ) {
+		var errors = {};
+		var $form  = $el.closest('form');
+		var $inputs = $form.find(':input');
+		var data   = can.deparam( $form.serialize() );
+
+		Temoa.fn.disable( $inputs );
+		$form.find('.error').empty();  // remove any previous errors
+
+		if ( Object.keys( errors ).length > 0 ) {
+			// client-side checking for user convenience.  The server will check
+			// for itself, of course.
+			Temoa.fn.enable( $inputs );
+			Temoa.fn.displayErrors( $form, errors );
+			return;
+		}
+
+		this.analysis.attr( data ).save(
+			function ( model ) {
+				Temoa.fn.enable( $inputs );
+				Temoa.fn.showStatus( 'Analysis successfully created.', 'info' );
+		}, function ( xhr ) {
+				Temoa.fn.enable( $inputs );
+				if ( xhr && xhr.responseJSON ) {
+					Temoa.fn.displayErrors( $form, xhr.responseJSON );
+				}
+		});
+	},
+	createNewCommodity: function ( CommodityObj, commodityOpts ) {
+		var $newDiv = $('<div>', {id: 'commodity_detail'} );
+		new CommodityDetail( $newDiv, {
+			username: Temoa.fn.getCookie().username || null,
+			analysis: this.analysis,
+			commodity: new CommodityObj( commodityOpts )
+		});
+		$('#commodity_detail').replaceWith( $newDiv );
+	},
+	'#NewCommodityDemand click': function ( $el ) {
+		var opts = {aId: this.analysis.id}
+		this.createNewCommodity( AnalysisCommodityDemand, opts );
+	},
+	'#NewCommodityEmission click': function ( $el ) {
+		var opts = {aId: this.analysis.id, name: 'New Emission Commodity'}
+		this.createNewCommodity( AnalysisCommodityEmission, opts );
+	},
+	'#NewCommodityPhysical click': function ( $el ) {
+		var opts = {aId: this.analysis.id, name: 'New Physical Commodity'}
+		this.createNewCommodity( AnalysisCommodityPhysical, opts );
+	},
+	'[name="CommodityDemandRemove"] click': function ( $el, ev ) {
+		$el.closest( 'tr' ).data('commodity').destroy();
+	},
+	'[name="CommodityEmissionRemove"] click': function ( $el, ev ) {
+		$el.closest( 'tr' ).data('commodity').destroy();
+	},
+	'[name="CommodityPhysicalRemove"] click': function ( $el, ev ) {
+		$el.closest( 'tr' ).data('commodity').destroy();
+	},
+	'{AnalysisCommodityDemand} created': function ( list, ev, commodity ) {
+		this.analysis.commodity_demand.unshift( commodity.real_model );
+	},
+	'{AnalysisCommodityEmission} created' : function ( list, ev, commodity ) {
+		this.analysis.commodity_emission.unshift( commodity.real_model );
+	},
+	'{AnalysisCommodityPhysical} created' : function ( list, ev, commodity ) {
+		this.analysis.commodity_physical.unshift( commodity.real_model );
+	},
+});
+
+})();
+
+console.log( 'TemoaDB CommodityLists View loaded: ' + Date() );
