7f3cff2574f24ec5b4206b83998185fbbba21929
jfdecarolis <jdecarolis@gmail.com>
2015-07-27 12:03:44 +0100

    Update Temoa copyright and license to GPLv2
    This update brings Temoa into compliance with the NC State Office
    of Technology Transfer. Temoa is now licensed under GPLv2 and the
    copyright belongs to NC State University. In making this update,
    I considered a more permissive license, such as BSD, in order to
    provide more flexibility to future users. However, in the end,
    I believe that the model should remain in the public domain, esp.
    if it is used to inform public policy. Hence the choice of a
    copyleft license.

--- create_archive.sh
+++ create_archive.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# Temoa - Tools for Energy Model Optimization and Analysis
-#   linear optimization; least cost; dynamic system visualization
-#
-# Copyright (C) 2011-2014  Kevin Hunter, Joseph DeCarolis
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Affero General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
-#
-# Developers of this script will check out a complete copy of the GNU Affero
-# General Public License in the file COPYING.txt.  Users uncompressing this from
-# an archive may not have received this license file.  If not, see
-# <http://www.gnu.org/licenses/>.
+# Tools for Energy Model Optimization and Analysis (Temoa): 
+# An open source framework for energy systems optimization modeling
+# 
+# Copyright (C) 2015,  NC State University
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# A complete copy of the GNU General Public License v2 (GPLv2) is available 
+# in LICENSE.txt.  Users uncompressing this from an archive may not have 
+# received this license file.  If not, see <http://www.gnu.org/licenses/>.
 
-
-# This script creates the 'temoa.py' zip archive/executable.  I do not know if
-# it is portable, but I know that it correctly creates the executable with the
-# Ubuntu Linux distribution's version of Zip, Info-ZIP.
+# This script creates the 'temoa.py' zip archive/executable. It creates the 
+#executable with the Ubuntu Linux distribution's version of Zip, Info-ZIP.
 
 # To create the archive, execute with no arguments (in this directory), or
 # with '--save'.  The save argument merely saves the temporary zip file from
--- temoa_model/temoa_rules.py
+++ temoa_model/temoa_rules.py
@@ -1,22 +1,22 @@
 """
-Temoa - Tools for Energy Model Optimization and Analysis
-  linear optimization; least cost; dynamic system visualization
+Tools for Energy Model Optimization and Analysis (Temoa): 
+An open source framework for energy systems optimization modeling
 
-Copyright (C) 2011-2014  Kevin Hunter, Joseph DeCarolis
+Copyright (C) 2015,  NC State University
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Affero General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option) any
-later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-Developers of this script will check out a complete copy of the GNU Affero
-General Public License in the file COPYING.txt.  Users uncompressing this from
-an archive may not have received this license file.  If not, see
-<http://www.gnu.org/licenses/>.
+A complete copy of the GNU General Public License v2 (GPLv2) is available 
+in LICENSE.txt.  Users uncompressing this from an archive may not have 
+received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from temoa_lib import *
