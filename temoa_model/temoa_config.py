832f0448ed374984ea841c6591e499f3b756f30e
Hadi Eshraghi <EnergyMan@Hadis-MacBook-Pro-2.local>
2019-10-01 10:36:19 -0400

    Add StorageInit parameter

--- temoa_model/temoa_config.py
+++ temoa_model/temoa_config.py
@@ -151,6 +151,7 @@ def db_2_dat(ifile, ofile, options):
 		['param','CapacityCredit',            '',                    '',             2],
 		['param','RampUp',                    '',                    '',             1],
 		['param','RampDown',                  '',                    '',             1],
+		['param','StorageInit',      	      '',                    '',             1],
 		['param','StorageDuration',           '',                    '',             1]]
 
 	with open(ofile, 'w') as f:
