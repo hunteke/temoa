89610f2ed807f707f1e167e8ecb65cce6cefa1fc
Hadi Eshraghi <heshrag@ncsu.edu>
2020-09-01 15:23:46 -0400

    Modify data_processing files These files are used for data visualization as well as generating the output Excel file.

--- data_processing/DB_to_Excel.py
+++ data_processing/DB_to_Excel.py
@@ -5,9 +5,10 @@ import getopt
 import xlwt
 from xlwt import easyxf 
 from collections import defaultdict
-
+from IPython import embed as IP
 def make_excel(ifile, ofile, scenario):
 	tech = defaultdict(list)
+	regions = set()	
 	tech_set = set()
 	sector = set()
 	period = []
@@ -76,6 +77,11 @@ def make_excel(ifile, ofile, scenario):
 						tech[x].append(val[0])
 						tech_set.add(val[0])
 
+		if k is "Output_VFlow_Out" :
+			cur.execute("SELECT DISTINCT region FROM "+k)
+			for val in cur :
+				regions.add(val[0])
+
 		if k is "Output_Emissions" :
 			cur.execute("SELECT DISTINCT emissions_comm FROM "+k)
 			for val in cur :
@@ -100,91 +106,95 @@ def make_excel(ifile, ofile, scenario):
 
 	for scene in scenario :	
 		book.append(xlwt.Workbook(encoding="utf-8"))
-		for z in sector :
-			for a in tables.keys() :
-				if z is '0' :
-					sheet_name = str(tables[a][0])
-					if a is "Output_Costs" :
+		
+		for r in regions:
+			for z in sector :
+				for a in tables.keys() :
+					if z is '0' :
+						sheet_name = str(tables[a][0])
+						if a is "Output_Costs" :
+							flag2 = '1'
+						if a is "Output_Emissions" :
+							flag1 = '1'
+					elif (a is "Output_Costs" and flag2 is None) :
+						sheet_name = str(tables[a][0])
 						flag2 = '1'
-					if a is "Output_Emissions" :
+					elif (a is "Output_Emissions" and flag1 is None) :
+						sheet_name = str(tables[a][0])
 						flag1 = '1'
-				elif (a is "Output_Costs" and flag2 is None) :
-					sheet_name = str(tables[a][0])
-					flag2 = '1'
-				elif (a is "Output_Emissions" and flag1 is None) :
-					sheet_name = str(tables[a][0])
-					flag1 = '1'
-				elif (a is "Output_Costs" and flag2 is not None) or (a is "Output_Emissions" and flag1 is not None) :
-					continue
-				else :
-					sheet_name = str(tables[a][0])+"_"+str(z)
-				sheet.append(book[book_no].add_sheet(sheet_name))
-				if a is "Output_Emissions" and flag1 is '1':
-					for col in range(0, len(header_emiss)) :
-						sheet[i].write(row, col, header_emiss[col], ostyle_header)
-						sheet[i].col(col).width_in_pixels = 3300
-					row += 1
-					for x in tech_set :
-						for q in emiss :
+					elif (a is "Output_Costs" and flag2 is not None) or (a is "Output_Emissions" and flag1 is not None) :
+						continue
+					else :
+						sheet_name = str(tables[a][0])+"_"+str(z)+"_"+str(r)
+					sheet.append(book[book_no].add_sheet(sheet_name))
+					if a is "Output_Emissions" and flag1 is '1':
+						for col in range(0, len(header_emiss)) :
+							sheet[i].write(row, col, header_emiss[col], ostyle_header)
+							sheet[i].col(col).width_in_pixels = 3300
+						row += 1
+						for x in tech_set :
+							for q in emiss :
+								sheet[i].write(row, 0, x, ostyle)
+								sheet[i].write(row, 1, q, ostyle)
+								for y in period :
+									cur.execute("SELECT sum("+tables[a][1]+") FROM "+a+" WHERE region is '"+r+"' and t_periods is '"+y+"' and scenario is '"+scene+"' and tech is '"+x+"' and emissions_comm is '"+q+"'")
+									xyz = cur.fetchone()
+									if xyz[0] is not None :
+										sheet[i].write(row, count+2, float(xyz[0]), ostyle)
+									else :
+										sheet[i].write(row, count+2, '-', ostyle)
+									count += 1
+								row += 1
+								count = 0
+						row = 0
+						i += 1
+						flag1 = '2'
+					elif a is "Output_Costs" and flag2 is '1':
+						for col in range(0, len(header_v)) :
+							sheet[i].write(row, col, header_v[col], ostyle_header)
+							sheet[i].col(col).width_in_pixels = 3300
+						row += 1
+						for x in tech_set :			
+							cur.execute("SELECT output_name, vintage, "+tables[a][1]+" FROM "+a+" WHERE region is '"+r+"' and scenario is '"+scene+"' and tech is '"+x+"'")
+							for xyz in cur :
+								if xyz[0] is not None :
+									sheet[i].write(row, 0, x, ostyle)
+									sheet[i].write(row, count+1, xyz[0], ostyle)
+									sheet[i].write(row, count+2, xyz[1], ostyle)
+									sheet[i].write(row, count+3, xyz[2], ostyle)
+								else :
+									sheet[i].write(row, 0, x, ostyle)
+									sheet[i].write(row, count+1, '-', ostyle)
+									sheet[i].write(row, count+2, '-', ostyle)
+									sheet[i].write(row, count+3, '-', ostyle)
+								row += 1
+							count = 0
+						row = 0
+						i += 1
+						flag2 = '2'
+					elif (a is "Output_Costs" and flag2 is '2') or (a is "Output_Emissions" and flag1 is '2'):
+						pass
+					elif a is not "Output_V_Capacity":
+						for col in range(0, len(header)) :
+							sheet[i].write(row, col, header[col], ostyle_header)
+							sheet[i].col(col).width_in_pixels = 3300
+						row += 1
+						for x in tech[z] :
 							sheet[i].write(row, 0, x, ostyle)
-							sheet[i].write(row, 1, q, ostyle)
 							for y in period :
-								cur.execute("SELECT sum("+tables[a][1]+") FROM "+a+" WHERE t_periods is '"+y+"' and scenario is '"+scene+"' and tech is '"+x+"' and emissions_comm is '"+q+"'")
+								cur.execute("SELECT sum("+tables[a][1]+") FROM "+a+" WHERE region is '"+r+"' and t_periods is '"+y+"' and scenario is '"+scene+"' and tech is '"+x+"'")
 								xyz = cur.fetchone()
 								if xyz[0] is not None :
-									sheet[i].write(row, count+2, float(xyz[0]), ostyle)
+									sheet[i].write(row, count+1, float(xyz[0]), ostyle)
 								else :
-									sheet[i].write(row, count+2, '-', ostyle)
+									sheet[i].write(row, count+1, '-', ostyle)
 								count += 1
 							row += 1
 							count = 0
-					row = 0
-					i += 1
-					flag1 = '2'
-				elif a is "Output_Costs" and flag2 is '1':
-					for col in range(0, len(header_v)) :
-						sheet[i].write(row, col, header_v[col], ostyle_header)
-						sheet[i].col(col).width_in_pixels = 3300
-					row += 1
-					for x in tech_set :			
-						cur.execute("SELECT output_name, vintage, "+tables[a][1]+" FROM "+a+" WHERE scenario is '"+scene+"' and tech is '"+x+"'")
-						for xyz in cur :
-							if xyz[0] is not None :
-								sheet[i].write(row, 0, x, ostyle)
-								sheet[i].write(row, count+1, xyz[0], ostyle)
-								sheet[i].write(row, count+2, xyz[1], ostyle)
-								sheet[i].write(row, count+3, xyz[2], ostyle)
-							else :
-								sheet[i].write(row, 0, x, ostyle)
-								sheet[i].write(row, count+1, '-', ostyle)
-								sheet[i].write(row, count+2, '-', ostyle)
-								sheet[i].write(row, count+3, '-', ostyle)
-							row += 1
-						count = 0
-					row = 0
-					i += 1
-					flag2 = '2'
-				elif (a is "Output_Costs" and flag2 is '2') or (a is "Output_Emissions" and flag1 is '2'):
-					pass
-				elif a is not "Output_V_Capacity":
-					for col in range(0, len(header)) :
-						sheet[i].write(row, col, header[col], ostyle_header)
-						sheet[i].col(col).width_in_pixels = 3300
-					row += 1
-					for x in tech[z] :
-						sheet[i].write(row, 0, x, ostyle)
-						for y in period :
-							cur.execute("SELECT sum("+tables[a][1]+") FROM "+a+" WHERE t_periods is '"+y+"' and scenario is '"+scene+"' and tech is '"+x+"'")
-							xyz = cur.fetchone()
-							if xyz[0] is not None :
-								sheet[i].write(row, count+1, float(xyz[0]), ostyle)
-							else :
-								sheet[i].write(row, count+1, '-', ostyle)
-							count += 1
-						row += 1
-						count = 0
-					row = 0
-					i += 1
+						row = 0
+						i += 1
+				
+		
 		if len(scenario) is 1:
 			book[book_no].save(ofile+".xls")
 		else :
