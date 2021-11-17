f4661d1e70b72f4e3cbb471900da223f7d6f0cb0
Aranya Venkatesh <40495316+aranyavenkatesh@users.noreply.github.com>
2021-11-16 19:55:42 -0500

    Parse techinputsplitaverage in temoa_myopic Added the new techinputsplitaverage to the temoa_myopic file where rows containing future years are deleted.

--- temoa_model/temoa_myopic.py
+++ temoa_model/temoa_myopic.py
@@ -56,7 +56,7 @@ def myopic_db_generator_solver ( self ):
 
     # group 1 consists of non output tables in which "periods" is a column name 
     tables_group1 = ['CostFixed','CostVariable','Demand','EmissionLimit','MaxActivity','MaxCapacity', \
-                     'MinActivity','MinCapacity','TechInputSplit','TechOutputSplit','CapacityCredit','MinGenGroupTarget']
+                     'MinActivity','MinCapacity','TechInputSplit','TechInputSplitAverage','TechOutputSplit','CapacityCredit','MinGenGroupTarget']
     # group 2 consists of non output tables in which "vintage" is a column name except for CostFixed and CostVariable (taken care of above)
     tables_group2 = ['CapacityFactorProcess','CostInvest','DiscountRate', \
                      'Efficiency','EmissionActivity','ExistingCapacity','LifetimeProcess']    
