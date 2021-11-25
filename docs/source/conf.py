e6d5a8f4cd4542801dfc804048f18f17b9eb0b88
Joseph F. Decarolis <jdecarolis@gmail.com>
2021-11-25 08:56:06 -0500

    Streamline package installation for documentation
    Add sphinx-related dependencies to the environment file. Remove spelling module, which requires some manual installations. Note for future: latex package still needs to be installed manually. Tried adding miktex to the environment file but it fails.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,7 +40,6 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.graphviz',
-    'sphinxcontrib.spelling',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
